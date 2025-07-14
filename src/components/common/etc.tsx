0151425dd1252deb1b261d4d6affeedb30acff1f
teakyungg <kak0502@naver.com>
2025-07-14 22:23:22 +0900

    ✨sparkles: public login compoent create

--- src/components/common/etc.tsx
+++ src/components/common/etc.tsx
@@ -22,3 +22,21 @@ export function InputIdSmall() {
     />
   );
 }
+
+// 아이디 입력(기본)
+export function LoginButtonDefault() {
+  return (
+    <button className="w-[28.8125rem] h-[3.5625rem] bg-primary-800 text-white rounded-[.25rem] hover:opacity-[70%] semibold-24 font-pretendard">
+      로그인
+    </button>
+  );
+}
+
+// 아이디 입력(480px)
+export function LoginButtonSmall() {
+  return (
+    <button className="w-[17.75rem] h-[2.6875rem] bg-primary-800 text-white rounded-[.25rem] hover:opacity-[70%] semibold-14 font-pretendard">
+      로그인
+    </button>
+  );
+}

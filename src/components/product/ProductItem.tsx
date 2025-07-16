f25c41068dc92ad2342b8ae0be225b3abde4694d
오서현 <bbibbi1118@naver.com>
2025-07-16 21:29:25 +0900

    🐛 fix svg bug

--- src/components/product/ProductItem.tsx
+++ src/components/product/ProductItem.tsx
@@ -1,42 +1,72 @@
 import '@app/globals.css';
 import Image from 'next/image';
 import productKeroro from 'assets/images/productKeroro.jpg';
-import like from 'assets/icons/heart-empty.svg';
-import likeFilled from 'assets/icons/heart-fill.svg';
+import { Heart, HeartIcon } from 'lucide-react';
+// import like from 'assets/icons/heart-empty.svg';
+// import likeFilled from 'assets/icons/heart-fill.svg';
 
 // 샛별 담당
 
-export function ProductItem(){
+export function ProductItem() {
   return (
-    <div className='flex flex-col gap-[15px] tablet:gap-5 normal-10 h-full w-[215px] tablet:w-[222px] laptop:w-[260px] desktop:w-[285px]'>
-      
+    <div className="flex flex-col gap-[15px] tablet:gap-5 normal-10 h-full w-[215px] tablet:w-[222px] laptop:w-[260px] desktop:w-[285px]">
       {/* 썸네일 */}
-      <div className='relative'>
-        <Image className='w-full h-[194px] rounded-2xl object-cover' src={ productKeroro } alt='/'/>
-          <div className='absolute group right-4 bottom-4'>
-            <Image className='group-hover:hidden' src={ like } alt={'/'}/>
-            <Image className='hidden group-hover:block' src={ likeFilled } alt={'/'}/>
-          </div>
+      <div className="relative">
+        <Image className="w-full h-[194px] rounded-2xl object-cover" src={productKeroro} alt="/" />
+        <div className="absolute group right-4 bottom-4">
+          <HeartIcon className="w-[30px] h-[30px] hover:text-red-500 hover:fill-red-500" strokeWidth={1.5} />
+        </div>
       </div>
 
-      <div className='space-y-2.5 tablet:space-y-5'>
-
+      <div className="space-y-2.5 tablet:space-y-5">
         {/* 달성율, 디데이 */}
-        <div className='flex gap-2.5 font-bold tablet:text-[20px] laptop:text-[24px]'>
-          <p className='text-primary-800 '>5,394% 달성</p>
-          <p className='text-font-400'>D-7</p>
+        <div className="flex gap-2.5 font-bold tablet:text-[20px] laptop:text-[24px]">
+          <p className="text-primary-800 ">5,394% 달성</p>
+          <p className="text-font-400">D-7</p>
         </div>
 
         {/* 제품명, 가격 */}
-        <div className='tablet:text-[12px] laptop:text-[14px]'>
-          <p className='text-font-900 font-bold '>개구리 중사 케로케로케로케로 티셔츠</p>
-          <p className='text-font-900'>500,000원</p>
+        <div className="tablet:text-[12px] laptop:text-[14px]">
+          <p className="text-font-900 font-bold ">개구리 중사 케로케로케로케로 티셔츠</p>
+          <p className="text-font-900">500,000원</p>
         </div>
-        
+
         {/* 회사명 */}
-        <p className='text-font-400 tablet:text-[12px] laptop:text-[14px]'>(주) 1더하기1은귀요미</p>
-      
+        <p className="text-font-400 tablet:text-[12px] laptop:text-[14px]">(주) 1더하기1은귀요미</p>
       </div>
     </div>
+  );
+}
+
+export function MainProdutItem() {
+  return (
+    <>
+      <div className="flex flex-col gap-[15px] tablet:gap-5 normal-10 h-full w-[215px] tablet:w-[222px] laptop:w-[260px] desktop:w-[285px]">
+        {/* 썸네일 */}
+        <div className="relative">
+          <Image className="w-full h-[194px] rounded-2xl object-cover" src={productKeroro} alt="/" />
+          <div className="absolute group right-4 bottom-4">
+            <HeartIcon className="w-[30px] h-[30px] hover:text-red-500 hover:fill-red-500" strokeWidth={1.5} />
+          </div>
+        </div>
+
+        <div className="space-y-2.5 tablet:space-y-5">
+          {/* 달성율, 디데이 */}
+          <div className="flex gap-2.5 font-bold tablet:text-[20px] laptop:text-[24px]">
+            <p className="text-primary-800 ">5,394% 달성</p>
+            <p className="text-font-400">D-7</p>
+          </div>
+
+          {/* 제품명, 가격 */}
+          <div className="tablet:text-[12px] laptop:text-[14px]">
+            <p className="text-font-900 font-bold ">개구리 중사 케로케로케로케로 티셔츠</p>
+            <p className="text-font-900">500,000원</p>
+          </div>
+
+          {/* 회사명 */}
+          <p className="text-font-400 tablet:text-[12px] laptop:text-[14px]">(주) 1더하기1은귀요미</p>
+        </div>
+      </div>
+    </>
   );
 }

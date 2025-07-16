86b1fe62b8afd26508f00b5d13e3028e2169460a
jungwoo0601 <pjw1346799@naver.com>
2025-07-16 15:34:23 +0900

    💄style: fix style(CategoryBar)

--- src/components/button/RoundedBtn.tsx
+++ src/components/button/RoundedBtn.tsx
@@ -19,33 +19,67 @@ export function CategoryButton() {
 export function CategoryBar() {
   return (
     <div className="flex flex-wrap gap-4">
-      <button className="medium-14 inline-flex items-center justify-center h-[39px] px-[32px] py-[11px] gap-1 rounded-[50px] bg-white text-gray-900 border border-secondary-200 hover:bg-primary-800 hover:text-white">
-        푸드
-      </button>
-      <button className="medium-14 inline-flex items-center justify-center h-[39px] px-[32px] py-[11px] gap-1 rounded-[50px] bg-white text-gray-900 border border-secondary-200 hover:bg-primary-800 hover:text-white">
-        의류 · 잡화
-      </button>
-      <button className="medium-14 inline-flex items-center justify-center h-[39px] px-[32px] py-[11px] gap-1 rounded-[50px] bg-white text-gray-900 border border-secondary-200 hover:bg-primary-800 hover:text-white">
-        홈 · 리빙
-      </button>
-      <button className="medium-14 inline-flex items-center justify-center h-[39px] px-[32px] py-[11px] gap-1 rounded-[50px] bg-white text-gray-900 border border-secondary-200 hover:bg-primary-800 hover:text-white">
-        문구
-      </button>
-      <button className="medium-14 inline-flex items-center justify-center h-[39px] px-[32px] py-[11px] gap-1 rounded-[50px] bg-white text-gray-900 border border-secondary-200 hover:bg-primary-800 hover:text-white">
-        뷰티 · 향수
-      </button>
-      <button className="medium-14 flex items-center justify-center h-[39px] px-[32px] py-[11px] rounded-[50px] bg-primary-800 text-white">
-        특별기획 · 시즌 기획
-      </button>
-      <button className="medium-14 inline-flex items-center justify-center h-[39px] px-[32px] py-[11px] gap-1 rounded-[50px] bg-white text-gray-900 border border-secondary-200 hover:bg-primary-800 hover:text-white">
-        테크
-      </button>
-      <button className="medium-14 inline-flex items-center justify-center h-[39px] px-[32px] py-[11px] gap-1 rounded-[50px] bg-white text-gray-900 border border-secondary-200 hover:bg-primary-800 hover:text-white">
-        키즈
-      </button>
-      <button className="medium-14 inline-flex items-center justify-center h-[39px] px-[32px] py-[11px] gap-1 rounded-[50px] bg-white text-gray-900 border border-secondary-200 hover:bg-primary-800 hover:text-white">
-        게임
-      </button>
+      <div className="flex flex-wrap gap-4">
+        <button className="medium-14 inline-flex items-center justify-center h-[39px] px-[32px] py-[11px] gap-1 rounded-[50px] bg-white text-gray-900 border border-secondary-200 hover:bg-primary-800 hover:text-white">
+          푸드
+        </button>
+        <button className="medium-14 inline-flex items-center justify-center h-[39px] px-[32px] py-[11px] gap-1 rounded-[50px] bg-white text-gray-900 border border-secondary-200 hover:bg-primary-800 hover:text-white">
+          의류 · 잡화
+        </button>
+        <button className="medium-14 inline-flex items-center justify-center h-[39px] px-[32px] py-[11px] gap-1 rounded-[50px] bg-white text-gray-900 border border-secondary-200 hover:bg-primary-800 hover:text-white">
+          홈 · 리빙
+        </button>
+        <button className="medium-14 inline-flex items-center justify-center h-[39px] px-[32px] py-[11px] gap-1 rounded-[50px] bg-white text-gray-900 border border-secondary-200 hover:bg-primary-800 hover:text-white">
+          문구
+        </button>
+        <button className="medium-14 inline-flex items-center justify-center h-[39px] px-[32px] py-[11px] gap-1 rounded-[50px] bg-white text-gray-900 border border-secondary-200 hover:bg-primary-800 hover:text-white">
+          뷰티 · 향수
+        </button>
+        <button className="medium-14 inline-flex items-center justify-center h-[39px] px-[32px] py-[11px] gap-1 rounded-[50px] bg-white text-gray-900 border border-secondary-200 hover:bg-primary-800 hover:text-white">
+          특별기획 · 시즌 기획
+        </button>
+        <button className="medium-14 inline-flex items-center justify-center h-[39px] px-[32px] py-[11px] gap-1 rounded-[50px] bg-white text-gray-900 border border-secondary-200 hover:bg-primary-800 hover:text-white">
+          테크
+        </button>
+        <button className="medium-14 inline-flex items-center justify-center h-[39px] px-[32px] py-[11px] gap-1 rounded-[50px] bg-white text-gray-900 border border-secondary-200 hover:bg-primary-800 hover:text-white">
+          키즈
+        </button>
+        <button className="medium-14 inline-flex items-center justify-center h-[39px] px-[32px] py-[11px] gap-1 rounded-[50px] bg-white text-gray-900 border border-secondary-200 hover:bg-primary-800 hover:text-white">
+          게임
+        </button>
+      </div>
+
+      <br />
+
+      <div className="flex flex-wrap gap-4">
+        <button className="medium-14 flex items-center justify-center h-[39px] px-[32px] py-[11px] rounded-[50px] bg-primary-800 text-white">
+          푸드
+        </button>
+        <button className="medium-14 flex items-center justify-center h-[39px] px-[32px] py-[11px] rounded-[50px] bg-primary-800 text-white">
+          의류 · 잡화
+        </button>
+        <button className="medium-14 flex items-center justify-center h-[39px] px-[32px] py-[11px] rounded-[50px] bg-primary-800 text-white">
+          홈 · 리빙
+        </button>
+        <button className="medium-14 flex items-center justify-center h-[39px] px-[32px] py-[11px] rounded-[50px] bg-primary-800 text-white">
+          문구
+        </button>
+        <button className="medium-14 flex items-center justify-center h-[39px] px-[32px] py-[11px] rounded-[50px] bg-primary-800 text-white">
+          뷰티 · 향수
+        </button>
+        <button className="medium-14 flex items-center justify-center h-[39px] px-[32px] py-[11px] rounded-[50px] bg-primary-800 text-white">
+          특별기획 · 시즌 기획
+        </button>
+        <button className="medium-14 flex items-center justify-center h-[39px] px-[32px] py-[11px] rounded-[50px] bg-primary-800 text-white">
+          테크
+        </button>
+        <button className="medium-14 flex items-center justify-center h-[39px] px-[32px] py-[11px] rounded-[50px] bg-primary-800 text-white">
+          키즈
+        </button>
+        <button className="medium-14 flex items-center justify-center h-[39px] px-[32px] py-[11px] rounded-[50px] bg-primary-800 text-white">
+          게임
+        </button>
+      </div>
     </div>
   );
 }

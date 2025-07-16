7a0710b597d17c2999f6d7080efd6642a914774b
Saetbyeol Bae <stella97415@gmail.com>
2025-07-17 01:01:44 +0900

    💄 add CircleCheckbox component

---
+++ src/components/checkbox/SquareCheckbox.tsx
@@ -0,0 +1,31 @@
+import '@app/globals.css';
+import Checkbox from '@assets/icons/checkbox.svg';
+import UnCheckbox from '@assets/icons/uncheckbox.svg';
+
+type CheckSquareProps = {
+  prop: string | null;
+};
+
+{/* 체크 됨 */}
+export function CheckSquare({ prop }: CheckSquareProps) {
+  return(
+    <div className='flex gap-1.5'>
+      <button>
+        <UnCheckbox className='w-[12px]'/>
+      </button>
+      <p className='medium-10 tablet:text-[15px]'>{ prop }</p>
+    </div>
+  );
+}
+
+{/* 체크 안 됨 */}
+export function UncheckSquare({ prop }: CheckSquareProps){
+  return(
+    <div className='flex gap-1.5'>
+      <button>
+        <Checkbox className='w-[12px]' />
+      </button>
+      <p className='medium-10 tablet:text-[15px]'>{ prop }</p>
+    </div>
+  );
+}

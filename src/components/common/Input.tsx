3e346a943d98efb8e32a7e331131a25943620093
kimlog126 <jisu11260@gmail.com>
2025-07-17 09:51:42 +0900

    💄 add input serach question #31

--- src/components/common/Input.tsx
+++ src/components/common/Input.tsx
@@ -1,3 +1,5 @@
+import { Search } from 'lucide-react';
+
 // 아이디 입력(기본)
 export function InputIdDefault() {
   return (
@@ -17,5 +19,19 @@ export function InputIdMobile() {
       className="w-[17.75rem] px-[.9375rem] py-[1.1875rem] border-[.125rem] border-font-400 rounded-[.5rem] box-content font-pretendard"
       placeholder="아이디 입력"
     />
+  );
+}
+
+// 문의하기 검색 바 (반응형 o)
+export function InputSearchQuestion() {
+  return (
+    <>
+      <div className="flex bg-bg border border-font-400 justify-between w-[603px] items-center mobile:w-[227px] mobile:px-3 mobile:py-2.5 mobile:rounded-md tablet:w-[331px] tablet:px-5 tablet:py-3.5 laptop:w-[547px] laptop:px-6 laptop:py-[18px] laptop:rounded-lg desktop:w-[603px]">
+        <p className="normal-14 text-font-400 mobile:text-[12px] tablet:text-[12px] laptop:text-[14px]">
+          펀드림의 궁금한 점을 검색해 보세요.
+        </p>
+        <Search className="stroke-font-400 mobile:w-3.5 mobile:h-3.5 laptop:w-6 laptop:h-6" />
+      </div>
+    </>
   );
 }

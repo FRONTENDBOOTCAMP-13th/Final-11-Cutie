88d986faf2cd10e145ad6d3ab1f0faa1fcdf4184
오서현 <bbibbi1118@naver.com>
2025-07-17 17:14:52 +0900

    💄 yes or no button (white, blue)

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,13 @@
+import { NoButtonblue, NoButtonWhite, YesButtonblue, YesButtonWhite } from '@components/button/SquareBtn';
+
 export default function Home() {
   return (
     <div>
       <p className="bold-24">인덱스 페이지입니다</p>
+      <YesButtonWhite />
+      <YesButtonblue />
+      <NoButtonWhite />
+      <NoButtonblue />
     </div>
   );
 }

81a097813e12aefbb10cddcd8fd6b602e202dabd
kimlog126 <jisu11260@gmail.com>
2025-07-17 12:50:50 +0900

    🐛 fix input viewport #31

--- src/components/common/Input.tsx
+++ src/components/common/Input.tsx
@@ -26,11 +26,9 @@ export function InputIdMobile() {
 export function InputSearchQuestion() {
   return (
     <>
-      <div className="flex bg-bg border border-font-400 justify-between w-[603px] items-center mobile:w-[227px] mobile:px-3 mobile:py-2.5 mobile:rounded-md tablet:w-[331px] tablet:px-5 tablet:py-3.5 laptop:w-[547px] laptop:px-6 laptop:py-[18px] laptop:rounded-lg desktop:w-[603px]">
-        <p className="normal-14 text-font-400 mobile:text-[12px] tablet:text-[12px] laptop:text-[14px]">
-          펀드림의 궁금한 점을 검색해 보세요.
-        </p>
-        <Search className="stroke-font-400 mobile:w-3.5 mobile:h-3.5 laptop:w-6 laptop:h-6" />
+      <div className="flex bg-bg border border-font-400 justify-between items-center w-[227px] px-3 py-2.5 rounded-md tablet:w-[331px] tablet:px-5 tablet:py-3.5 laptop:w-[547px] laptop:px-6 laptop:py-[18px] laptop:rounded-lg desktop:w-[603px]">
+        <p className="normal-12 text-font-400 laptop:text-[14px]">펀드림의 궁금한 점을 검색해 보세요.</p>
+        <Search className="stroke-font-400 w-3.5 h-3.5 laptop:w-6 laptop:h-6" />
       </div>
     </>
   );

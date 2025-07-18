8792a7c4a96f88c1cace2d0e3a24f549e5670e40
jungwoo0601 <pjw1346799@naver.com>
2025-07-18 17:18:51 +0900

    🐛 fix: BlueCategoryBar component

--- src/components/button/RoundedBtn.tsx
+++ src/components/button/RoundedBtn.tsx
@@ -35,7 +35,7 @@ export function CategoryButton() {
   );
 }
 
-// 카테고리 버튼 리스트
+// 카테고리 버튼 리스트 (호버가능, 반응형)
 export function CategoryBar() {
   return (
     <div className="flex flex-wrap gap-4">
@@ -151,35 +151,107 @@ export function CategoryBar() {
   );
 }
 
-// 파란색 카테고리 버튼 리스트
+// 파란색 카테고리 버튼 리스트(반응형)
 export function BlueCategoryBar() {
   return (
     <div className="flex flex-wrap gap-4">
-      <button className="medium-14 flex items-center justify-center h-[39px] px-[32px] py-[11px] rounded-[50px] bg-primary-800 text-white">
+      <button
+        className="
+          flex items-center justify-center
+          rounded-[50px] border
+          bg-primary-800 text-white
+          medium-14 px-[32px] py-[11px]
+          laptop:text-[16px] laptop:px-[32px] laptop:py-[11px]
+        "
+      >
         푸드
       </button>
-      <button className="medium-14 flex items-center justify-center h-[39px] px-[32px] py-[11px] rounded-[50px] bg-primary-800 text-white">
+      <button
+        className="
+          flex items-center justify-center
+          rounded-[50px] border
+          bg-primary-800 text-white
+          medium-14 px-[32px] py-[11px]
+          laptop:text-[16px] laptop:px-[32px] laptop:py-[11px]
+        "
+      >
         의류 · 잡화
       </button>
-      <button className="medium-14 flex items-center justify-center h-[39px] px-[32px] py-[11px] rounded-[50px] bg-primary-800 text-white">
+      <button
+        className="
+          flex items-center justify-center
+          rounded-[50px] border
+          bg-primary-800 text-white
+          medium-14 px-[32px] py-[11px]
+          laptop:text-[16px] laptop:px-[32px] laptop:py-[11px]
+        "
+      >
         홈 · 리빙
       </button>
-      <button className="medium-14 flex items-center justify-center h-[39px] px-[32px] py-[11px] rounded-[50px] bg-primary-800 text-white">
+      <button
+        className="
+          flex items-center justify-center
+          rounded-[50px] border
+          bg-primary-800 text-white
+          medium-14 px-[32px] py-[11px]
+          laptop:text-[16px] laptop:px-[32px] laptop:py-[11px]
+        "
+      >
         문구
       </button>
-      <button className="medium-14 flex items-center justify-center h-[39px] px-[32px] py-[11px] rounded-[50px] bg-primary-800 text-white">
+      <button
+        className="
+          flex items-center justify-center
+          rounded-[50px] border
+          bg-primary-800 text-white
+          medium-14 px-[32px] py-[11px]
+          laptop:text-[16px] laptop:px-[32px] laptop:py-[11px]
+        "
+      >
         뷰티 · 향수
       </button>
-      <button className="medium-14 flex items-center justify-center h-[39px] px-[32px] py-[11px] rounded-[50px] bg-primary-800 text-white">
+      <button
+        className="
+          flex items-center justify-center
+          rounded-[50px] border
+          bg-primary-800 text-white
+          medium-14 px-[32px] py-[11px]
+          laptop:text-[16px] laptop:px-[32px] laptop:py-[11px]
+        "
+      >
         특별기획 · 시즌 기획
       </button>
-      <button className="medium-14 flex items-center justify-center h-[39px] px-[32px] py-[11px] rounded-[50px] bg-primary-800 text-white">
+      <button
+        className="
+          flex items-center justify-center
+          rounded-[50px] border
+          bg-primary-800 text-white
+          medium-14 px-[32px] py-[11px]
+          laptop:text-[16px] laptop:px-[32px] laptop:py-[11px]
+        "
+      >
         테크
       </button>
-      <button className="medium-14 flex items-center justify-center h-[39px] px-[32px] py-[11px] rounded-[50px] bg-primary-800 text-white">
+      <button
+        className="
+          flex items-center justify-center
+          rounded-[50px] border
+          bg-primary-800 text-white
+          medium-14 px-[32px] py-[11px]
+          laptop:text-[16px] laptop:px-[32px] laptop:py-[11px]
+        "
+      >
         키즈
       </button>
-      <button className="medium-14 flex items-center justify-center h-[39px] px-[32px] py-[11px] rounded-[50px] bg-primary-800 text-white">
+      <button
+        className="
+          flex items-center justify-center
+          rounded-[50px] border
+          bg-primary-800 text-white
+          medium-14 px-[32px] py-[11px]
+          laptop:text-[16px] laptop:px-[32px] laptop:py-[11px]
+        "
+      >
         게임
       </button>
     </div>

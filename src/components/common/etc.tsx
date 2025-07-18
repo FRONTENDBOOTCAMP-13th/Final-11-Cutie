d601476814bc643441e982eefe33728d7c8caf71
teakyungg <kak0502@naver.com>
2025-07-19 00:59:35 +0900

    💄 fixed: addfunding font

--- src/components/common/etc.tsx
+++ src/components/common/etc.tsx
@@ -5,42 +5,36 @@ import Right from '@assets/icons/chevron-right.svg';
 
 // 특별기획/시즌기획(기본)
 export function SpecialPlanDefault() {
-  return <p className="bold-24 w-[11.875rem] h-[1.875rem]">특별기획/시즌기획</p>;
+  return <p className="bold-24 h-[30px]">특별기획/시즌기획</p>;
 }
 
 // 특별기획/시즌기획(768px)
 export function SpecialPlanTablet() {
-  return <p className="bold-20 w-[10rem] h-[1.5625rem]">특별기획/시즌기획</p>;
+  return <p className="bold-20 h-[25px]">특별기획/시즌기획</p>;
 }
 
 // 특별기획/시즌기획(480px)
 export function SpecialPlanMobile() {
-  return <p className="semibold-17 w-[8.25rem] h-[1.375rem]">특별기획/시즌기획</p>;
+  return <p className="semibold-18 h-[22px]">특별기획/시즌기획</p>;
 }
 
 // 더 많은 펀딩보기(1440, 1280)
 export function AddfundingDefault() {
   return (
-    <Link
-      href="/"
-      className="group justify-between flex w-[123px] h-[20px]  text-font-400 hover:text-primary-800 medium-14"
-    >
-      더 많은 펀딩 보기
-      <Right className="w-[20px] h-[20px] text-font-400 group-hover:text-primary-800" />
-    </Link>
+    <div className="flex gap-[10px] h-[20px] medium-16 text-font-400 hover:text-primary-800 hover:fill-primary-800">
+      <Link href="#">더 많은 펀딩 보기</Link>
+      <Right className="w-[20px] h-[20px]" />
+    </div>
   );
 }
 
 // 더 많은 펀딩보기(768, 480)
 export function AddfundingTablet() {
   return (
-    <Link
-      href="/"
-      className="group justify-between flex w-[120px] h-[16px] text-font-400 hover:text-primary-800 medium-12"
-    >
-      더 많은 펀딩 보기
-      <Right className="w-[18px] h-[16px] text-font-400 group-hover:text-primary-800" />
-    </Link>
+    <div className="flex gap-[8px] h-[20px] medium-14 text-font-400 hover:text-primary-800 hover:fill-primary-800">
+      <Link href="#">더 많은 펀딩 보기</Link>
+      <Right className="w-[18px] h-[18px]" />
+    </div>
   );
 }
 

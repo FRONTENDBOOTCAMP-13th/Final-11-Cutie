814bb97d43bfcddcc3f1551feb27fbe91b827467
jungwoo0601 <pjw1346799@naver.com>
2025-07-19 00:56:34 +0900

    ✨ add : ReviewCard component)

--- src/components/review/ReviewSummary.tsx
+++ src/components/review/ReviewSummary.tsx
@@ -1,24 +1,22 @@
-//후기 테이블
-export function ReviewTable() {
-  return (
-    <div>
-      {/* 호버가능한 후기 테이블 */}
-      <button className="flex items-center w-[247px] h-[16px] border border-primary-800 rounded-[4px] overflow-hidden font-pretendard font-normal hover:bg-primary-50 text-[5px]">
-        <div className="w-[31px] h-full flex items-center justify-center border-r border-primary-800">1</div>
-        <div className="flex-1 h-full flex items-center pl-[6px] border-r border-primary-800">
-          색깔이 생각보다 어둡네요
-        </div>
-        <div className="w-[38px] h-full flex items-center justify-center">2024.05.05</div>
-      </button>
+import ProfileIcon from '@assets/icons/profile.svg';
 
-      {/* 호버된 후기 테이블 UI */}
-      <button className="flex items-center w-[247px] h-[16px] border bg-primary-50 border-primary-800 rounded-[4px] overflow-hidden font-pretendard font-normal text-[5px]">
-        <div className="w-[31px] h-full flex items-center justify-center border-r border-primary-800">1</div>
-        <div className="flex-1 h-full flex items-center pl-[6px] border-r border-primary-800">
-          색깔이 생각보다 어둡네요
+// 후기 테이블
+export function ReviewCard() {
+  return (
+    <div className="flex flex-col w-[440px] mobile:w-[568px] tablet:w-[880px] laptop:w-[984px] bg-primary-50">
+      <div className="flex items-center gap-[10px]">
+        <ProfileIcon className="w-[35px] h-[35px]" />
+
+        {/* 닉네임 + 별점 */}
+        <div className="flex flex-col">
+          <span className="bold-14 laptop:text-[16px] text-font-900">닉네임</span>
+          <div className="flex items-center ml-[-3px]">
+            <span className="text-yellow-400 normal-14 laptop:text-[16px] ">⭐</span>
+            <span className="normal-16 text-font-900">5.0</span>
+          </div>
         </div>
-        <div className="w-[38px] h-full flex items-center justify-center">2024.05.05</div>
-      </button>
+      </div>
+      <p className="normal-14 laptop:text-[16px] text-font-900 mt-[10px]">대만족 합니다.</p>
     </div>
   );
 }

eca3fc34fb7eb5e2d33fd30aa8b837f54981ef6e
Saetbyeol Bae <stella97415@gmail.com>
2025-07-18 23:11:23 +0900

    🔥 remove test code

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,7 @@
-import { CheckSquareBtn, SquareBtnBlue, SquareBtnWhite, UnCheckSquareBtn } from "@components/button/SquareBtn";
-import ProductForm from "@components/product/ProductForm";
-
-
 export default function Home() {
   return (
     <div>
       <p className="bold-24">인덱스 페이지입니다</p>
-      <ProductForm />
-      <UnCheckSquareBtn prop='인증하기' />
-      <CheckSquareBtn prop='인증하기' />
-      <SquareBtnWhite />
-      <SquareBtnBlue/>
     </div>
   );
 }

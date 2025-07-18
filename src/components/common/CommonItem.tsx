c3d2f71a87e5389c93ed3c59500d8be14ef9b1f5
Saetbyeol Bae <stella97415@gmail.com>
2025-07-18 23:10:05 +0900

    💄 change font size, viewport  CommonItem

--- src/components/common/CommonItem.tsx
+++ src/components/common/CommonItem.tsx
@@ -4,8 +4,8 @@ import { CircleQuestionMark } from "lucide-react";
 export function FaqWhite(){
   return(
     <div>
-      <button className="bg-white min-w-[215px] h-[15px] mobile:w-[341px] mobile:h-[41px] tablet:w-[346px] flex items-center gap-1 normal-10 mobile:text-[12px] tablet:text-[14px] text-font-900">
-        <CircleQuestionMark className="stroke-primary-800 w-[12px] mobile:w-[19px]"/>
+      <button className="bg-white w-[214px] h-[15px] mobile:w-[342px] mobile:h-[41px] laptop:w-[346px] flex items-center gap-1 normal-14 mobile:text-[14px] laptop:text-[16px] text-font-900 hover:bg-primary-50">
+        <CircleQuestionMark className="stroke-primary-800 w-[15px] mobile:w-[19px]"/>
         <span>[결제]</span>
         <span>할부 결제가 가능한가요?</span>
       </button>
@@ -17,8 +17,8 @@ export function FaqWhite(){
 export function FaqBlue(){
   return(
     <div>
-      <button className="bg-primary-50 min-w-[215px] h-[15px] mobile:w-[341px] mobile:h-[41px] tablet:w-[346px] flex items-center gap-1 normal-10 mobile:text-[12px] tablet:text-[14px] text-font-900">
-        <CircleQuestionMark className="stroke-primary-800 w-[12px] mobile:w-[19px]"/>
+      <button className="bg-primary-50 w-[214px] h-[15px] mobile:w-[342px] mobile:h-[41px] laptop:w-[346px] flex items-center gap-1 normal-14 mobile:text-[14px] laptop:text-[16px] text-font-900">
+        <CircleQuestionMark className="stroke-primary-800 w-[15px] mobile:w-[19px]"/>
         <span>[결제]</span>
         <span>할부 결제가 가능한가요?</span>
       </button>
@@ -30,7 +30,7 @@ export function FaqBlue(){
 export function QnaItemWhite(){
   return(
     <div>
-    <button className="normal-14 rounded-sm bg-white text-left w-[492px] h-[37px] ">
+    <button className="normal-14 rounded-sm bg-white text-left w-[492px] h-[37px] hover:bg-primary-50">
       일반문의
     </button>
     </div>

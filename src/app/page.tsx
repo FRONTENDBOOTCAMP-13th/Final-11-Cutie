609a10c47ed436b43db9448c9da322cec4beb191
오서현 <bbibbi1118@naver.com>
2025-07-19 18:21:24 +0900

    ✅  delete test

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,21 +1,7 @@
-import { CheckFinalAmount, OrderedProductComponent } from '@components/address/DeliveryAddress';
-import { NoButtonblue, NoButtonWhite, YesButtonblue, YesButtonWhite } from '@components/button/SquareBtn';
-import { See, ToggleSwitchBig } from '@components/common/etc';
-import { SelectBox, SelectBoxDrop } from '@components/menu/Category';
-import { MainProdutItem } from '@components/product/ProductItem';
-
 export default function Home() {
   return (
     <div>
       <p className="bold-24">인덱스 페이지입니다</p>
-      <YesButtonWhite />
-      <YesButtonblue />
-      <NoButtonWhite />
-      <NoButtonblue />
-      <See />
-      <ToggleSwitchBig />
-      <SelectBox />
-      <SelectBoxDrop />
     </div>
   );
 }

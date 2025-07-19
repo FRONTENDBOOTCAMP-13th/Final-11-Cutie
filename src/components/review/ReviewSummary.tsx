1b4cd50c869c3b825c961f1c07a3a067ea168ac8
jungwoo0601 <pjw1346799@naver.com>
2025-07-20 00:45:19 +0900

    🐛fix: ReviewListItem component

--- src/components/review/ReviewSummary.tsx
+++ src/components/review/ReviewSummary.tsx
@@ -5,15 +5,25 @@ import ReviewProductIcon from '@assets/images/reviewProduct.jpg';
 // 후기 테이블
 export function ReviewListItem() {
   return (
-    <div className="flex flex-col w-[440px] mobile:w-[568px] tablet:w-[880px] laptop:w-[984px] bg-primary-50">
+    <div
+      className="
+      flex flex-col 
+      w-[440px] 
+      mobile:w-[568px]
+      tablet:w-[880px] 
+      laptop:w-[984px] 
+      bg-primary-50 
+      [@media(min-width:480px)_and_(max-width:768px)]:w-[470px]
+      [@media(min-width:768px)_and_(max-width:900px)]:w-[680px]
+      px-[16px]
+    "
+    >
       <div className="flex items-center gap-[10px]">
         <ProfileIcon className="w-[35px] h-[35px]" />
-
-        {/* 닉네임 + 별점 */}
         <div className="flex flex-col">
           <span className="bold-14 laptop:text-[16px] text-font-900">닉네임</span>
           <div className="flex items-center ml-[-3px]">
-            <span className="text-yellow-400 normal-14 laptop:text-[16px] ">⭐</span>
+            <span className="text-yellow-400 normal-14 laptop:text-[16px]">⭐</span>
             <span className="normal-16 text-font-900">5.0</span>
           </div>
         </div>

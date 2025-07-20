cef6017e54a9708183f266a1b9eb27542dda54dc
kimlog126 <jisu11260@gmail.com>
2025-07-20 22:45:24 +0900

    💄 add qna page setting

---
+++ src/app/(simple)/qna/[id]/page.tsx
@@ -0,0 +1,7 @@
+export default function QnaDetailPage() {
+  return (
+    <>
+      <h1 className="bold-24">문의하기 상세 페이지입니다</h1>
+    </>
+  );
+}
--- src/app/(simple)/qna/page.tsx
+++ src/app/(simple)/qna/page.tsx
@@ -1,4 +1,4 @@
-export default function QnAPage() {
+export default function QnaPage() {
   return (
     <>
       <h1 className="bold-24">문의하기 페이지입니다</h1>

297f13c94b9e0d95ec1859f69b279c44c4eda9bf
오서현 <bbibbi1118@naver.com>
2025-07-20 15:43:45 +0900

    🔥 delete test

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,3 @@
-import { Header } from '@components/common/Header';
-import { ShippingAddressModal } from '@components/modal/card/CardModal';
-
 export default function Home() {
-  return (
-    <div>
-      <Header />
-      <ShippingAddressModal />
-    </div>
-  );
+  return <div></div>;
 }

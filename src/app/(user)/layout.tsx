6e303d6d9ffeb1321e943615b4e89540eff46749
kimlog126 <jisu11260@gmail.com>
2025-07-22 02:33:23 +0900

    💄 fix media query

--- src/app/(user)/layout.tsx
+++ src/app/(user)/layout.tsx
@@ -2,7 +2,7 @@ import Logo from '../../../public/icons/logo.svg';
 
 export default function UserLayout({ children }: { children: React.ReactNode }) {
   return (
-    <div className="flex justify-center min-w-[464px] mobile:min-w-[745px]">
+    <div className="flex justify-center min-w-[320px] mobile:min-w-[745px]">
       <div className="flex flex-col w-[416px] mobile:w-[665px] h-fit mt-[77px] mb-[67px] bg-primary-50 rounded-[35px] mobile:rounded-[48px] tablet:rounded-[58px] px-[42px] py-[92px] mobile:px-[55px] mobile:py-[135px] gap-9 text-font-900 items-center">
         <h1>
           <Logo width={120} height={43} />

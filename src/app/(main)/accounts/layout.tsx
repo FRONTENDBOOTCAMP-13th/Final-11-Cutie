50ee441bb0cf8361ef4481d6b9dfe5908aa8f749
teakyungg <kak0502@naver.com>
2025-07-22 15:57:33 +0900

    💄 add: mypage 768px, 1280px

--- src/app/(main)/accounts/layout.tsx
+++ src/app/(main)/accounts/layout.tsx
@@ -5,15 +5,21 @@ export default function AccountLayout({ children }: { children: React.ReactNode 
     <>
       {/* 마이페이지 공통 레이아웃 작성 */}
       {/* 공통 컴포넌트*/}
-      <div className="p-[24px]">
+      <div className="p-[24px] tablet:grid tablet:grid-cols-[0.7fr_0.3fr] tablet:gap-[13px] tablet:p-[40px]">
+        {/* 프로필, 상품 소개 */}
         <div className="flex flex-col border-[0.5px] rounded-[14px] border-primary-800">
+          {/* 프로필 */}
           <Profile />
 
+          {/* 상품 소개 */}
           <section className="p-[10px]">
             <h2 className="sr-only">My Page</h2>
             {children}
           </section>
         </div>
+
+        {/* 알림 */}
+        <Alert />
       </div>
     </>
   );
@@ -23,14 +29,47 @@ function Profile() {
   return (
     <div className="flex justify-between px-[25px] py-[15px] border-b-[1px] border-primary-800">
       <div className="flex gap-[10px] items-center">
-        <ProfileImg width={27} height={27} />
-        <span className="normal-14 font-[700]">홍길동</span>
+        <ProfileImg width={27} height={27} className="tablet:w-[40px] h-[40px]" />
+        <span className="normal-14 font-[700] tablet:text-[20px]">홍길동</span>
       </div>
       <div className="flex gap-[11px] normal-14 font-[600]">
         <button className="cursor-pointer px-[7px] py-[4px] border-[1px] rounded-[8px] text-primary-800">
           프로필 편집
         </button>
-        <button className="cursor-pointer px-[7px] py-[4px] border-[1px] rounded-[8px] text-error">알림</button>
+        <button className="cursor-pointer px-[7px] py-[4px] border-[1px] rounded-[8px] text-error tablet:hidden">
+          알림
+        </button>
+      </div>
+    </div>
+  );
+}
+
+function Alert() {
+  return (
+    <div className="hidden tablet:flex flex-col gap-[8px] noraml-14 font-[600] border border-primary-800 px-[6px] py-[11px] rounded-[8px] bg-primary-50">
+      <span className="px-[24px] py-[6px] border-[1px] border-error w-fit rounded-[13px] text-error bg-white">
+        알림
+      </span>
+      <AlertMessage />
+      <AlertMessage />
+      <AlertMessage />
+      <AlertMessage />
+      <AlertMessage />
+      <AlertMessage />
+      <AlertMessage />
+      <AlertMessage />
+      <AlertMessage />
+    </div>
+  );
+}
+
+function AlertMessage() {
+  return (
+    <div className="flex flex-col gap-[4px] border px-[8px] py-[6px] border-error bg-white rounded-[6px]">
+      <span>[알림]</span>
+      <div className="flex flex-col gap-[1.5px]">
+        <span>후원이 완료되었습니다.</span>
+        <span className="text-secondary-200">2023.05.08</span>
       </div>
     </div>
   );
--- src/app/(main)/accounts/page.tsx
+++ src/app/(main)/accounts/page.tsx
@@ -1,7 +1,7 @@
 export default function Account() {
   return (
     <>
-      <h1 className="bold-24 h-[500px] bg-amber-300">마이페이지입니다</h1>
+      <h1 className="bold-24 bg-amber-300">마이페이지입니다. 여기에 상품 페이지가 들어가야합니다.</h1>
     </>
   );
 }

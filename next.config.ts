f74c25deef4c150b2452b93b4b72900de33bad65
kimlog126 <jisu11260@gmail.com>
2025-07-22 18:39:07 +0900

    🔧 add next config image setting

--- next.config.ts
+++ next.config.ts
@@ -1,14 +1,26 @@
 import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-  /* config options here */
-  reactStrictMode: true,
-  turbopack: {
-    rules: {
-      '*.svg': {
-        loaders: ['@svgr/webpack'],
-        as: '*.js',
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'http',
+        hostname: 'localhost',
+        port: '',
+        pathname: '/market/files/**',
       },
+      {
+        protocol: 'https',
+        hostname: 'fesp-api.koyeb.app',
+        port: '',
+        pathname: '/market/files/**',
+      },
+    ],
+  },
+  /* config options here */
+  experimental: {
+    serverActions: {
+      bodySizeLimit: '10mb', // 서버액션에 전달하는 바디 크기(기본은 1MB)
     },
   },
 };

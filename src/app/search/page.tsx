470141f059d85601ca8881dc10db5bc4ffdd2e31
오서현 <bbibbi1118@naver.com>
2025-07-22 14:21:28 +0900

    💄 SearchPage

--- src/app/search/page.tsx
+++ src/app/search/page.tsx
@@ -1,3 +1,18 @@
+import { Search } from 'lucide-react';
+
 export default function SearchPage() {
-  return <h1 className="bold-24">검색하기 페이지입니다</h1>;
+  return (
+    <>
+      {/* 검색 바 */}
+      <input type="text" defaultValue="검색어를 입력해주세요." />
+      <Search />
+
+      {/* API로 날짜 가져와야 함 */}
+      <section>
+        <span>인기 검색어</span>
+        <span>2025.08.08</span>
+      </section>
+      {/* 인기 목록 */}
+    </>
+  );
 }

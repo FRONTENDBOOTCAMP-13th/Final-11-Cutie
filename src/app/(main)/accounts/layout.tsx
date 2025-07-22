82dab7770ad66a159a6848f7185f85545740430f
teakyungg <kak0502@naver.com>
2025-07-22 15:05:32 +0900

    💄 add: mypage 480px

--- src/app/(main)/accounts/layout.tsx
+++ src/app/(main)/accounts/layout.tsx
@@ -1,9 +1,37 @@
+import ProfileImg from '@assets/icons/profile.svg';
+
 export default function AccountLayout({ children }: { children: React.ReactNode }) {
   return (
     <>
       {/* 마이페이지 공통 레이아웃 작성 */}
-      {/* 공통 탭 컴포넌트는 제작 필요 */}
-      <main>{children}</main> {/* 각 탭의 컨텐츠 연결될 자리*/}
+      {/* 공통 컴포넌트*/}
+      <div className="p-[24px]">
+        <div className="flex flex-col border-[0.5px] rounded-[14px] border-primary-800">
+          <Profile />
+
+          <section className="p-[10px]">
+            <h2 className="sr-only">My Page</h2>
+            {children}
+          </section>
+        </div>
+      </div>
     </>
+  );
+}
+
+function Profile() {
+  return (
+    <div className="flex justify-between px-[25px] py-[15px] border-b-[1px] border-primary-800">
+      <div className="flex gap-[10px] items-center">
+        <ProfileImg width={27} height={27} />
+        <span className="normal-14 font-[700]">홍길동</span>
+      </div>
+      <div className="flex gap-[11px] normal-14 font-[600]">
+        <button className="cursor-pointer px-[7px] py-[4px] border-[1px] rounded-[8px] text-primary-800">
+          프로필 편집
+        </button>
+        <button className="cursor-pointer px-[7px] py-[4px] border-[1px] rounded-[8px] text-error">알림</button>
+      </div>
+    </div>
   );
 }
--- src/app/(main)/accounts/page.tsx
+++ src/app/(main)/accounts/page.tsx
@@ -1,7 +1,7 @@
 export default function Account() {
   return (
     <>
-      <h1 className="bold-24">마이페이지입니다</h1>
+      <h1 className="bold-24 h-[500px] bg-amber-300">마이페이지입니다</h1>
     </>
   );
 }

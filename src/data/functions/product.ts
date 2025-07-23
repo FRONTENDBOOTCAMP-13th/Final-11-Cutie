4806777d8b0db0b14963c81dd132cd0a0ad3c042
kimlog126 <jisu11260@gmail.com>
2025-07-23 15:02:01 +0900

    🔧 fix get url

--- src/data/functions/product.ts
+++ src/data/functions/product.ts
@@ -6,10 +6,12 @@ const CLIENT_ID = process.env.NEXT_PUBLIC_CLIENT_ID || '';
 
 export async function getProducts(): ApiResPromise<Iproduct[]> {
   try {
-    const res = await fetch(`${API_URL}/products/products?limit=12&page=1`, {
+    const res = await fetch(`${API_URL}/products`, {
       headers: {
         'Client-Id': CLIENT_ID,
       },
+      // 캐시상태 넣어놔서 업데이트가 느림
+      // 빠르게 확인용 => cache: 'no-store', 으로 교체
       cache: 'force-cache',
     });
     return res.json();

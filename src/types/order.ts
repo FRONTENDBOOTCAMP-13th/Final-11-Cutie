3a004f1808d04690bf5e0c397f9b7ce9959b6bbf
kimlog126 <jisu11260@gmail.com>
2025-07-24 14:56:41 +0900

    🔧 add order type

--- src/types/order.ts
+++ src/types/order.ts
@@ -1,17 +1,41 @@
-import { Iproduct } from './product';
+import { IproductImg } from './product';
 
-export interface ISellerOrder {
+// 주문한 상품
+export interface IOrderProduct {
+  _id: number;
+  state: string;
+  name: string;
+  image: IproductImg;
+  quantity: number;
+  price: number;
+  review_id: number;
+}
+
+// 주문 가격
+export interface IOrderCost {
+  products: number;
+  shippingFees: number;
+  discount: {
+    products: number;
+    shippingFees: number;
+  };
+  total: number;
+}
+
+// 주문 배송지
+export interface IOrderAddress {
+  name: string; // 배송지명 (ex. "회사", "집")
+  value: string; // 실제 주소 문자열
+}
+
+// 상세 주문 정보
+export interface ISellerOrderDetail {
   _id: number;
   user_id: number;
-  product_id: number;
-  quantity: number;
-  totalPrice: number;
-  status: 'paid' | 'shipping' | 'done' | 'cancelled';
+  state: string; // 전체 주문 상태
+  products: IOrderProduct[];
+  cost: IOrderCost;
+  address: IOrderAddress;
   createdAt: string;
   updatedAt: string;
-  product: Iproduct;
-  user?: {
-    name: string;
-    email: string;
-  };
 }

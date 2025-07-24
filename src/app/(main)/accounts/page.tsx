3d648a12a64a3de462043a4d61da8baec4507ce4
jungwoo0601 <pjw1346799@naver.com>
2025-07-24 10:24:10 +0900

    💄fix: accounts whitebox (page.tsx)

--- src/app/(main)/accounts/page.tsx
+++ src/app/(main)/accounts/page.tsx
@@ -1,5 +1,7 @@
 export default function Account() {
   return (
-    <div className="overflow-y-auto bg-primary-50 h-[737px] laptop:h-[1110px] rounded-b-[5px] p-[12px] mobile:p-[16px] tablet:p-[13px] laptop:p-[15px]"></div>
+    <div className=" bg-primary-50 h-[737px] laptop:h-[1110px] rounded-b-[5px] p-[12px] mobile:p-[16px] tablet:p-[13px] laptop:p-[15px]">
+      <div className="bg-white p-10 h-full overflow-y-auto"></div>
+    </div>
   );
 }

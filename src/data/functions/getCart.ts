5bf1bf307362786fb44b24bbc19c9350e98b52c3
kimlog126 <jisu11260@gmail.com>
2025-07-24 13:51:09 +0900

    🔧 add get cart item function

---
+++ src/data/functions/getCart.ts
@@ -0,0 +1,37 @@
+'use server';
+
+import { IcartProductRes } from '@models/cart';
+
+const API_URL = process.env.NEXT_PUBLIC_API_URL;
+const CLIENT_ID = process.env.NEXT_PUBLIC_CLIENT_ID || '';
+
+/**
+ * 장바구니 목록을 조회합니다.
+ * @param accessToken - 로그인한 유저의 액세스 토큰
+ * @returns 장바구니 상품 목록과 비용 정보
+ */
+export async function getCartItems(accessToken: string): Promise<IcartProductRes> {
+  try {
+    const res = await fetch(`${API_URL}/cart`, {
+      method: 'GET',
+      headers: {
+        'Client-Id': CLIENT_ID,
+        Authorization: `Bearer ${accessToken}`,
+      },
+      // 캐시상태 넣어놔서 업데이트가 느림
+      // 빠르게 확인하려면 설정에서 캐시 삭제하기
+      cache: 'force-cache',
+    });
+
+    if (!res.ok) {
+      throw new Error('장바구니 목록 조회 실패');
+    }
+
+    return res.json();
+  } catch (err) {
+    console.error('getCartItems error:', err);
+    throw err;
+  }
+}
+
+

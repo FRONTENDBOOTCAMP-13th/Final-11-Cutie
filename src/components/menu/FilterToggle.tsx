b914377d67d00c759553992cf6c087dca1b4be0f
Saetbyeol Bae <stella97415@gmail.com>
2025-07-25 14:32:14 +0900

    🔥 remove FilterToggleOpen funtion

--- src/components/menu/FilterToggle.tsx
+++ src/components/menu/FilterToggle.tsx
@@ -3,23 +3,6 @@
 import { ChevronDown, ChevronUp } from 'lucide-react';
 import { useState } from 'react';
 
-// 필터 토글 펼친 상태
-export function FilterToggleOpen() {
-  return (
-    <>
-      <button
-        name="filter"
-        type="button"
-        className="flex w-[90px] p-[10px] border-1 border-font-400 items-center justify-between cursor-pointer"
-      >
-        {/* p 안 props 필요 */}
-        <p className="bold-14 text-font-400">추천순</p>
-        {/* svg 부분 props 필요 */}
-        <ChevronUp className="w-[20px] h-[14px] text-font-400 bg-white" />
-      </button>
-    </>
-  );
-}
 
 // 필터 토글 카테고리 (추천순, 인기순, 최신순, 마감임박순)
 // 선택된 것과 선택 중 반영 위한 props 필요

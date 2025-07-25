98dd7850a29b3184c4ef7d4f9de2236782092f3e
kimlog126 <jisu11260@gmail.com>
2025-07-25 11:34:55 +0900

    🚚 change my page tab location

--- src/app/(main)/accounts/layout.tsx
+++ src/app/(main)/accounts/layout.tsx
@@ -1,4 +1,5 @@
 import ProfileImg from '@assets/icons/profile.svg';
+import { ProductTabs } from '@components/tab/myPageTab';
 import Link from 'next/link';
 
 export default function AccountLayout({ children }: { children: React.ReactNode }) {
@@ -76,31 +77,6 @@ function AlertMessage() {
         <span>후원이 완료되었습니다.</span>
         <span className="text-secondary-200 normal-12">2023.05.08</span>
       </div>
-    </div>
-  );
-}
-
-// 마이페이지탭
-function ProductTabs() {
-  return (
-    <div className="bg-primary-50 font-pretendard w-full border-b border-primary-100">
-      <nav className="flex w-full ">
-        {['구매 내역', '펀드 페이지', '장바구니', '나의 후기'].map(tab => (
-          <button
-            key={tab}
-            className="group flex-1 min-w-0 text-center px-0 py-[1.2vw]
-                       overflow-hidden whitespace-nowrap
-                       cursor-pointer text-[3.3vw] mobile:text-[14px] tablet:text-[16px] 
-                       text-font-900 hover:text-primary-800 relative font-semibold"
-          >
-            <span>{tab}</span>
-            <span
-              className="absolute left-1/2 -translate-x-1/2 bottom-[2px] w-[80%] h-[0.6vw] 
-                             bg-primary-800 opacity-0 group-hover:opacity-100 transition-opacity duration-200 rounded-[2px]"
-            ></span>
-          </button>
-        ))}
-      </nav>
     </div>
   );
 }

7a81e27ce2d21960b1a03ff7bb40f8ca7266a0ba
kimlog126 <jisu11260@gmail.com>
2025-07-25 15:56:01 +0900

    ✨ add account tab

--- src/components/tab/myPageTab.tsx
+++ src/components/tab/myPageTab.tsx
@@ -1,21 +1,44 @@
-// 마이페이지탭
+'use client';
+
+import { usePathname, useRouter } from 'next/navigation';
+import { useEffect, useState } from 'react';
+
 export function ProductTabs() {
+  const router = useRouter();
+  const pathname = usePathname();
+
+  const tabs = [
+    { label: '구매 내역', path: '/accounts' },
+    { label: '펀드 페이지', path: '/accounts/fund' },
+    { label: '장바구니', path: '/accounts/cart' },
+    { label: '나의 후기', path: '/accounts/myReview' },
+  ];
+
+  const [selectedPath, setSelectedPath] = useState(pathname);
+
+  useEffect(() => {
+    setSelectedPath(pathname);
+  }, [pathname]);
+
   return (
     <div className="bg-primary-50 font-pretendard w-full border-b border-primary-100">
-      <nav className="flex w-full ">
-        {['구매 내역', '펀드 페이지', '장바구니', '나의 후기'].map(tab => (
+      <nav className="flex w-full relative">
+        {tabs.map(tab => (
           <button
-            key={tab}
-            className="group flex-1 min-w-0 text-center px-0 py-[1.2vw]
-                       overflow-hidden whitespace-nowrap
-                       cursor-pointer text-[3.3vw] mobile:text-[14px] tablet:text-[16px] 
-                       text-font-900 hover:text-primary-800 relative font-semibold"
+            key={tab.path}
+            onClick={() => router.push(tab.path)}
+            className={`group flex-1 min-w-0 text-center px-0 py-[1.2vw]
+              overflow-hidden whitespace-nowrap cursor-pointer
+              text-[3.3vw] mobile:text-[14px] tablet:text-[16px] semibold-14 relative
+              ${selectedPath === tab.path ? 'text-primary-800' : 'text-font-900'}`}
           >
-            <span>{tab}</span>
+            {tab.label}
+            {/* 밑줄 */}
             <span
-              className="absolute left-1/2 -translate-x-1/2 bottom-[2px] w-[80%] h-[0.6vw] 
-                             bg-primary-800 opacity-0 group-hover:opacity-100 transition-opacity duration-200 rounded-[2px]"
-            ></span>
+              className={`absolute left-1/2 -translate-x-1/2 bottom-[2px] w-[80%] h-[0.6vw] 
+                rounded-[2px] bg-primary-800 transition-opacity duration-200
+                ${selectedPath === tab.path ? 'opacity-100' : 'opacity-0 group-hover:opacity-100'}`}
+            />
           </button>
         ))}
       </nav>

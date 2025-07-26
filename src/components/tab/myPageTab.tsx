38d5c0194dca7f73cf7ef270972123563e47fc0f
kimlog126 <jisu11260@gmail.com>
2025-07-26 12:53:54 +0900

    ✨ fix route tab page

--- src/components/tab/myPageTab.tsx
+++ src/components/tab/myPageTab.tsx
@@ -1,10 +1,9 @@
 'use client';
 
-import { usePathname, useRouter } from 'next/navigation';
-import { useEffect, useState } from 'react';
+import Link from 'next/link';
+import { usePathname } from 'next/navigation';
 
 export function ProductTabs() {
-  const router = useRouter();
   const pathname = usePathname();
 
   const tabs = [
@@ -14,32 +13,25 @@ export function ProductTabs() {
     { label: '나의 후기', path: '/accounts/myReview' },
   ];
 
-  const [selectedPath, setSelectedPath] = useState(pathname);
-
-  useEffect(() => {
-    setSelectedPath(pathname);
-  }, [pathname]);
-
   return (
     <div className="bg-primary-50 font-pretendard w-full border-b border-primary-100">
       <nav className="flex w-full relative">
         {tabs.map(tab => (
-          <button
+          <Link
             key={tab.path}
-            onClick={() => router.push(tab.path)}
+            href={tab.path}
             className={`group flex-1 min-w-0 text-center px-0 py-[1.2vw]
               overflow-hidden whitespace-nowrap cursor-pointer
               text-[3.3vw] mobile:text-[14px] tablet:text-[16px] semibold-14 relative
-              ${selectedPath === tab.path ? 'text-primary-800' : 'text-font-900'}`}
+              ${pathname === tab.path ? 'text-primary-800' : 'text-font-900'}`}
           >
             {tab.label}
-            {/* 밑줄 */}
             <span
               className={`absolute left-1/2 -translate-x-1/2 bottom-[2px] w-[80%] h-[0.6vw] 
                 rounded-[2px] bg-primary-800 transition-opacity duration-200
-                ${selectedPath === tab.path ? 'opacity-100' : 'opacity-0 group-hover:opacity-100'}`}
+                ${pathname === tab.path ? 'opacity-100' : 'opacity-0 group-hover:opacity-100'}`}
             />
-          </button>
+          </Link>
         ))}
       </nav>
     </div>

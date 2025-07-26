c1910d69035292f5e5d4a5cf1718fe9f7a05bfb4
kimlog126 <jisu11260@gmail.com>
2025-07-26 21:13:20 +0900

    ✨ add review item modal

---
+++ src/components/modal/review/ReviewModal.tsx
@@ -0,0 +1,58 @@
+'use client';
+
+import { ChangeButton, ChangeButtonPrimary } from '@components/button/SquareBtn';
+import { X } from 'lucide-react';
+import { useEffect } from 'react';
+
+type reviewModalProps = {
+  onClose: () => void;
+  // num: number;
+  // title: string;
+  // content: string;
+};
+
+export function ReviewModal({ onClose }: reviewModalProps) {
+  useEffect(() => {
+    document.body.style.overflow = 'hidden';
+    return () => {
+      document.body.style.overflow = '';
+    };
+  }, []);
+
+  return (
+    <div className="fixed flex inset-0 items-center justify-center bg-font-900/70">
+      <div className="bg-primary-50 mobile:mt-20 tablet:mt-34 rounded-[8px] border border-primary-800 p-[24px] w-[300px] h-[361px] mobile:p-[40px] mobile:w-[450px] mobile:h-[536px] flex flex-col">
+        {/* 상단 */}
+        <div className="flex items-center justify-between mb-7">
+          {/* 숫자 */}
+          <span className="px-[12px] py-[4px] semibold-14 border border-primary-800 rounded-[13px] text-primary-800 bg-white">
+            1
+          </span>
+          {/* 닫기 버튼 */}
+          <button onClick={onClose} aria-label="모달 닫기">
+            <X className="text-font-900" />
+          </button>
+        </div>
+
+        {/* 본문 */}
+        <div className="flex-1 overflow-y-auto flex flex-col gap-[8px] semibold-16 mb-4 custom-scroll">
+          <span>[ 색깔이 생각보다 너무 어둡네요 ]</span>
+          <span className="medium-12 text-secondary-200">2024.05.05</span>
+          <span className="medium-14 text-font-900">사진이랑 달라요. 아쉽습니다.</span>
+        </div>
+
+        {/* 하단 버튼 */}
+        <div className="flex w-full justify-between gap-4">
+          <ChangeButton
+            label="삭제"
+            className="flex-1 px-4 [@media(min-width:250px)_and_(max-width:394px)]:text-[12px]"
+          />
+          <ChangeButtonPrimary
+            label="수정"
+            className="flex-1 px-4 [@media(min-width:250px)_and_(max-width:394px)]:text-[12px]"
+          />
+        </div>
+      </div>
+    </div>
+  );
+}
--- src/components/tab/categoryTab.tsx
+++ src/components/tab/categoryTab.tsx
@@ -1,7 +1,12 @@
 'use client';
+
 import { ChangeButton, ChangeButtonFill, CheckboxBtn } from '@components/button/SquareBtn';
 import { CartItem } from '@components/cart/CartItem';
 import { MapPin } from 'lucide-react';
+import { PurchaseHistoryItemWrapContainer } from '@components/product/ProductItem';
+import Link from 'next/link';
+import { useState } from 'react';
+import { ReviewModal } from '@components/modal/review/ReviewModal';
 
 export default function CartTab() {
   // 예시용 3개 상태
@@ -107,10 +112,6 @@ export default function CartTab() {
   );
 }
 
-import { PurchaseHistoryItemWrapContainer } from '@components/product/ProductItem';
-import Link from 'next/link';
-import { useState } from 'react';
-
 // 구매내역
 export function PurchaseHistoryTab() {
   return (
@@ -130,6 +131,9 @@ export function FundPageTab() {
 }
 
 export function MyReviewTab() {
+  const [showModal, setShowModal] = useState(false);
+  const openModal = () => setShowModal(true);
+  const closeModal = () => setShowModal(false);
   return (
     <>
       <section className="p-[10px]">
@@ -141,56 +145,39 @@ export function MyReviewTab() {
             <p className="bg-primary-800 px-2 hidden mobile:block mobile:min-w-24 py-1.5 text-white">날짜</p>
           </div>
 
-          {/* 리뷰 부분 */}
+          {/* 리뷰 리스트 부분 */}
           <div className="flex flex-col gap-3 normal-14 laptop:text-[16px] text-font-900 mt-3">
-            <div className="grid grid-cols-[auto_1fr_auto] items-center border rounded-lg overflow-hidden hover:bg-primary-50 cursor-pointer">
-              <div className="px-2 min-w-10 py-1.5">1</div>
-              <div className="border-x px-2 text-left py-1.5 truncate">색깔이 생각보다 어둡네요</div>
-              <div className="px-2 hidden mobile:block mobile:min-w-24 py-1.5">2025.08.08</div>
-            </div>
-            <div className="grid grid-cols-[auto_1fr_auto] items-center border rounded-lg overflow-hidden hover:bg-primary-50 cursor-pointer">
-              <div className="px-2 min-w-10 py-1.5">1</div>
-              <div className="border-x px-2 text-left py-1.5 truncate">색깔이 생각보다 어둡네요</div>
-              <div className="px-2 hidden mobile:block mobile:min-w-24 py-1.5">2025.08.08</div>
-            </div>
-            <div className="grid grid-cols-[auto_1fr_auto] items-center border rounded-lg overflow-hidden hover:bg-primary-50 cursor-pointer">
-              <div className="px-2 min-w-10 py-1.5">1</div>
-              <div className="border-x px-2 text-left py-1.5 truncate">색깔이 생각보다 어둡네요</div>
-              <div className="px-2 hidden mobile:block mobile:min-w-24 py-1.5">2025.08.08</div>
-            </div>
-            <div className="grid grid-cols-[auto_1fr_auto] items-center border rounded-lg overflow-hidden hover:bg-primary-50 cursor-pointer">
-              <div className="px-2 min-w-10 py-1.5">1</div>
-              <div className="border-x px-2 text-left py-1.5 truncate">색깔이 생각보다 어둡네요</div>
-              <div className="px-2 hidden mobile:block mobile:min-w-24 py-1.5">2025.08.08</div>
-            </div>
-            <div className="grid grid-cols-[auto_1fr_auto] items-center border rounded-lg overflow-hidden hover:bg-primary-50 cursor-pointer">
-              <div className="px-2 min-w-10 py-1.5">1</div>
-              <div className="border-x px-2 text-left py-1.5 truncate">색깔이 생각보다 어둡네요</div>
-              <div className="px-2 hidden mobile:block mobile:min-w-24 py-1.5">2025.08.08</div>
-            </div>
-            <div className="grid grid-cols-[auto_1fr_auto] items-center border rounded-lg overflow-hidden hover:bg-primary-50 cursor-pointer">
-              <div className="px-2 min-w-10 py-1.5">1</div>
-              <div className="border-x px-2 text-left py-1.5 truncate">색깔이 생각보다 어둡네요</div>
-              <div className="px-2 hidden mobile:block mobile:min-w-24 py-1.5">2025.08.08</div>
-            </div>
-            <div className="grid grid-cols-[auto_1fr_auto] items-center border rounded-lg overflow-hidden hover:bg-primary-50 cursor-pointer">
-              <div className="px-2 min-w-10 py-1.5">1</div>
-              <div className="border-x px-2 text-left py-1.5 truncate">색깔이 생각보다 어둡네요</div>
-              <div className="px-2 hidden mobile:block mobile:min-w-24 py-1.5">2025.08.08</div>
-            </div>
-            <div className="grid grid-cols-[auto_1fr_auto] items-center border rounded-lg overflow-hidden hover:bg-primary-50 cursor-pointer">
-              <div className="px-2 min-w-10 py-1.5">1</div>
-              <div className="border-x px-2 text-left py-1.5 truncate">색깔이 생각보다 어둡네요</div>
-              <div className="px-2 hidden mobile:block mobile:min-w-24 py-1.5">2025.08.08</div>
-            </div>
-            <div className="grid grid-cols-[auto_1fr_auto] items-center border rounded-lg overflow-hidden hover:bg-primary-50 cursor-pointer">
-              <div className="px-2 min-w-10 py-1.5">1</div>
-              <div className="border-x px-2 text-left py-1.5 truncate">색깔이 생각보다 어둡네요</div>
-              <div className="px-2 hidden mobile:block mobile:min-w-24 py-1.5">2025.08.08</div>
-            </div>
+            {[...Array(7)].map((_, index) => (
+              <div key={index} onClick={openModal} className="cursor-pointer">
+                <ReviewItem num={index + 1} title="색깔이 생각보다 어둡네요" date="2025.08.08" />
+              </div>
+            ))}
           </div>
         </div>
       </section>
+
+      {/* 모달 */}
+      {showModal && <ReviewModal onClose={closeModal} />}
+    </>
+  );
+}
+
+type reviewItemProps = {
+  num: number;
+  title: string;
+  date: string;
+};
+
+// 리뷰 항목
+// title, date db 연동 필요
+export function ReviewItem({ num, title, date }: reviewItemProps) {
+  return (
+    <>
+      <div className="grid grid-cols-[auto_1fr_auto] items-center border rounded-lg overflow-hidden hover:bg-primary-50 cursor-pointer">
+        <div className="px-2 min-w-10 py-1.5">{num}</div>
+        <div className="border-x px-2 text-left py-1.5 truncate">{title}</div>
+        <div className="px-2 hidden mobile:block mobile:min-w-24 py-1.5">{date}</div>
+      </div>
     </>
   );
 }

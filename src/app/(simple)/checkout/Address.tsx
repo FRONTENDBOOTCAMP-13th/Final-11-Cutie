545df91ccc8d80864e3badba04ecc7bd16a97787
teakyungg <kak0502@naver.com>
2025-07-27 17:16:25 +0900

    💄 add : Address input style

--- src/app/(simple)/checkout/Address.tsx
+++ src/app/(simple)/checkout/Address.tsx
@@ -1,26 +1,43 @@
-import { PreviewCheckboxWithLabel } from '@components/button/SquareBtn';
+import { ChangeButtonFill, PreviewCheckboxWithLabel } from '@components/button/SquareBtn';
 
-// 이거 각각 컴포넌트 별로 사이즈 맞게 수정하기
-
+/* 배송지 추가 모달 내용 */
 export function Address() {
   const innerPadding = `p-[24px] ` + `mobile:p-[40px] ` + `tablet:p-[40px] ` + `laptop:p-[43px] `;
   const innerWidth = `w-[300px] ` + `mobile:w-[450px] ` + `tablet:w-[684px] ` + `laptop:w-[684px] `;
 
   return (
-    <div className={innerPadding + innerWidth}>
+    <div className={`${innerPadding} ${innerWidth} flex flex-col gap-[20px]`}>
+      {/* 제목 */}
       <AddAddressTitle />
-      <SetReceiver />
-      <AddAddress />
-      <AddPhoneNumber />
-      <DefaultAddressAndPersonalInform />
+
+      <div className="flex flex-col gap-[12px]">
+        {/* 받는 사람 */}
+        <SetReceiver />
+
+        {/* 주소 */}
+        <AddAddress />
+
+        {/* 받는 사람 휴대폰 */}
+        <AddPhoneNumber />
+
+        {/* 기본배송지 등록, 개인정보 수집 및 이용 동의 */}
+        <DefaultAddressAndPersonalInform />
+      </div>
+
+      {/* 등록 완료 */}
+      <ChangeButtonFill label={'등록 완료'} className="w-full py-[16px]" />
     </div>
   );
 }
 
 /* 배송지 추가 타이틀 */
 function AddAddressTitle() {
+  const style = 'mobile:pb-5';
+  const textSize = 'normal-18 text-[17px] mobile:text-[20px] laptop:text-[24px]';
+  const textStyle = 'font-[700]';
+
   return (
-    <div className="flex flex-row justify-between bold-16 mobile:text-[20px] laptop:text-[24px] mobile:pb-5">
+    <div className={`${style} ${textStyle} ${textSize}`}>
       <p>배송지 추가</p>
     </div>
   );
@@ -28,13 +45,22 @@ function AddAddressTitle() {
 
 /* 받는 사람 입력창 */
 function SetReceiver() {
+  const sortStyle = 'flex flex-col gap-[10px]';
+  const textSize = 'normal-14 laptop:text-[16px]';
+  const textStyle = 'font-[600]';
+
+  const inputStyle = 'bg-bg border-secondary-200 border rounded-sm p-[9px] mobile:p-[12px] laptop:p-[15px]';
+  const inputTextSize = 'normal-12 mobile:text-[14px] laptop:text-[16px]';
+  const inputTextStyle = 'font-[400]';
+  const inputSize = 'w-full h-[32px] mobile:h-[43px] laptop:h-[51px]';
+
   return (
-    <div className="flex flex-col gap-2.5">
-      <p className="bold-14 laptop:text-[16px]">받는 사람</p>
+    <div className={`${sortStyle}`}>
+      <p className={`${textStyle} ${textSize}`}>받는 사람</p>
       <input
         type="tel"
         placeholder="받는 분 성함을 입력해주세요."
-        className="bg-bg border-secondary-200 border w-full h-[28px] mobile:h-[40px] laptop:h-[47px] p-2.5 rounded-sm normal-12 mobile:text-[14px] laptop:text-[16px]"
+        className={`${inputStyle} ${inputTextSize} ${inputTextStyle} ${inputSize}`}
       />
     </div>
   );
@@ -42,13 +68,22 @@ function SetReceiver() {
 
 /* 주소 추가 입력창 */
 function AddAddress() {
+  const sortStyle = 'flex flex-col gap-[10px]';
+  const textSize = 'normal-14 laptop:text-[16px]';
+  const textStyle = 'font-[600]';
+
+  const inputStyle = 'bg-bg border-secondary-200 border rounded-sm p-[9px] mobile:p-[12px] laptop:p-[15px]';
+  const inputTextSize = 'normal-12 mobile:text-[14px] laptop:text-[16px]';
+  const inputTextStyle = 'font-[400]';
+  const inputSize = 'w-full h-[32px] mobile:h-[43px] laptop:h-[51px]';
+
   return (
-    <div className="flex flex-col gap-2.5">
-      <p className="bold-14 laptop:text-[16px]">주소</p>
+    <div className={`${sortStyle}`}>
+      <p className={`${textStyle} ${textSize}`}>주소</p>
       <input
         type="tel"
         placeholder="받는 분 주소를 입력해 주세요."
-        className="bg-bg border-secondary-200 border w-full h-[28px] mobile:h-[40px] laptop:h-[47px] p-2.5 rounded-sm normal-12 mobile:text-[14px] laptop:text-[16px]"
+        className={`${inputStyle} ${inputTextSize} ${inputTextStyle} ${inputSize}`}
       />
     </div>
   );
@@ -56,13 +91,22 @@ function AddAddress() {
 
 /* 받는 사람 전화번호 입력창 */
 function AddPhoneNumber() {
+  const sortStyle = 'flex flex-col gap-[10px]';
+  const textSize = 'normal-14 laptop:text-[16px]';
+  const textStyle = 'font-[600]';
+
+  const inputStyle = 'bg-bg border-secondary-200 border rounded-sm p-[9px] mobile:p-[12px] laptop:p-[15px]';
+  const inputTextSize = 'normal-12 mobile:text-[14px] laptop:text-[16px]';
+  const inputTextStyle = 'font-[400]';
+  const inputSize = 'w-full h-[32px] mobile:h-[43px] laptop:h-[51px]';
+
   return (
-    <div className="flex flex-col gap-2.5">
-      <p className="bold-14 laptop:text-[16px]">받는 사람 휴대폰 번호</p>
+    <div className={`${sortStyle}`}>
+      <p className={`${textSize} ${textStyle}`}>받는 사람 휴대폰 번호</p>
       <input
         type="tel"
         placeholder="받는 분 휴대폰 번호를 입력해주세요."
-        className="bg-bg border-secondary-200 border w-full h-[28px] mobile:h-[40px] laptop:h-[47px] p-2.5 rounded-sm normal-12 mobile:text-[14px] laptop:text-[16px]"
+        className={`${inputStyle} ${inputTextSize} ${inputTextStyle} ${inputSize}`}
       />
     </div>
   );
@@ -70,8 +114,10 @@ function AddPhoneNumber() {
 
 /* 기본 배송지 등록, 개인정보 수집 및 이용 동의 */
 function DefaultAddressAndPersonalInform() {
+  const srotStyle = 'flex flex-col gap-[10px]';
+
   return (
-    <div className="flex flex-col gap-3 mobile:gap-6">
+    <div className={`${srotStyle}`}>
       <PreviewCheckboxWithLabel title={'기본 배송지로 등록'} />
       <PreviewCheckboxWithLabel title={'개인정보 수집 및 이용 동의'} />
     </div>

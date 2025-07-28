c5fc55a96581a2fbbe8ca0f275f9739bf33c5197
jungwoo0601 <pjw1346799@naver.com>
2025-07-28 23:39:10 +0900

    📝 change: profileClient to ProfileFunction (Page)

--- src/app/(main)/accounts/layout.tsx
+++ src/app/(main)/accounts/layout.tsx
@@ -1,4 +1,4 @@
-import ProfileClient from './components/ProfileClient';
+import ProfileClient from './components/ProfileFunction';
 import Alert from './components/Alert';
 import { MyPageTabs } from './components/MyPageTabs';
 

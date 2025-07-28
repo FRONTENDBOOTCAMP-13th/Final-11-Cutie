01211ac2e9954a9e072374b9094976e128855664
teakyungg <kak0502@naver.com>
2025-07-28 17:08:14 +0900

    ✨ add: projectPlan day setting library

--- src/app/(main)/products/new/detail/ProjectPlan.tsx
+++ src/app/(main)/products/new/detail/ProjectPlan.tsx
@@ -1,12 +1,44 @@
+// 이거 날짜 선택 스타일
+import '@mantine/core/styles.css';
+import '@mantine/dates/styles.css';
+import { MantineProvider } from '@mantine/core';
+import { useState } from 'react';
+import { DatePickerInput } from '@mantine/dates';
+import dayjs from 'dayjs';
+import 'dayjs/locale/ko';
 import { StarTitle } from '@components/common/etc';
-import { SelectBox } from '@components/menu/Category';
+
+dayjs.locale('ko');
 
 /* 프로젝트 진행 일정 */
 export function ProjectPlan() {
   return (
-    <div className="flex flex-col gap-[15px] flex-1 w-[50%] laptop:w-auto">
+    <MantineProvider>
+      <SlectDate />
+    </MantineProvider>
+  );
+}
+
+function SlectDate() {
+  // value에 선택한 날짜 들어있음
+  const [value, setValue] = useState<[string | null, string | null]>([null, null]);
+  const today = dayjs().startOf('day').toDate();
+
+  const year = today.getFullYear();
+  const month = today.getMonth() + 1;
+  const date = today.getDate();
+
+  return (
+    <div>
       <StarTitle title="프로젝트 진행 일정" />
-      <SelectBox isDropdown={true} mainText={'2025.08.08'} />
+      <DatePickerInput
+        locale="ko"
+        type="range"
+        placeholder={`${year}/${month}/${date}`}
+        value={value}
+        onChange={setValue}
+        minDate={today}
+      />
     </div>
   );
 }

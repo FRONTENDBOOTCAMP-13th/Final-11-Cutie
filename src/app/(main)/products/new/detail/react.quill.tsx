2043a39939e12353c204c7167e394be5cdc1bd52
teakyungg <kak0502@naver.com>
2025-07-29 11:45:37 +0900

    ✨ add: detail.page image

--- src/app/(main)/products/new/detail/react.quill.tsx
+++ src/app/(main)/products/new/detail/react.quill.tsx
@@ -8,7 +8,15 @@ const ReactQuill = dynamic(() => import('react-quill-new'), {
   loading: () => <p>Loading ...</p>,
 });
 
+const modules = {
+  toolbar: [
+    [{ header: [1, 2, false] }],
+    ['bold', 'italic', 'underline'],
+    ['image', 'code-block'], // 여기 'image' 버튼 추가
+  ],
+};
+
 /* ReactQuill */
 export function QuillWrapper() {
-  return <ReactQuill className="w-full h-[300px]" theme="snow" />;
+  return <ReactQuill modules={modules} className="w-full h-[300px]" theme="snow" />;
 }

af4b9cdfdb809246943d2a9630dedd9d1736dd80
jungwoo0601 <pjw1346799@naver.com>
2025-07-29 10:48:14 +0900

    💄add: Mainpage

--- src/app/(main)/page.tsx
+++ src/app/(main)/page.tsx
@@ -6,7 +6,7 @@ export default function Home() {
     <>
       <div className="flex justify-center">
         <main className="flex flex-col w-full">
-          <section className="w-full px-0 mobile:px-0 tablet:px-[90px] laptop:px-[120px]">
+          <section className="w-full px-0 mobile:px-0 tablet:px-[90px] tablet:mt-[64px] laptop:px-[120px] laptop:mt-[64px] ">
             <MainBanner />
           </section>
           <section className="flex flex-col mx-auto w-full my-6 mobile:my-10 tablet:my-16 px-[24px] mobile:px-[40px] tablet:px-[90px] laptop:px-[120px] max-w-[1280px] gap-5 mobile:gap-[25px] tablet:gap-[30px]">

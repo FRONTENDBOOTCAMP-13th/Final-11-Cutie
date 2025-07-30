498d697169363d777dcbd3f61835eb3e9ed1f9bb
오서현 <bbibbi1118@naver.com>
2025-07-30 16:25:59 +0900

    ✨ search

--- src/app/search/SearchFunction.tsx
+++ src/app/search/SearchFunction.tsx
@@ -1,45 +1,60 @@
-'use client';
+// 'use client';
 
-import { useState } from 'react';
-import { useRouter } from 'next/navigation';
-import { Search } from 'lucide-react';
+// // import { useEffect, useState } from 'react';
+// import { useRouter } from 'next/navigation';
+// import { Search } from 'lucide-react';
 
-export function SearchFunction() {
-  const router = useRouter();
-  const [keyword, setKeyword] = useState('');
+// export function SearchFunction() {
+//   const router = useRouter();
+//   const [keyword, setKeyword] = useState('');
+//   // const [products, setProducts] = useState <Product[] > {[]};
+//   //  const [filteredProducts, setFilteredProducts] = useState<Product[]>([]);
 
-  const handleSearch = (e: React.KeyboardEvent<HTMLInputElement>) => {
-    // console.log('검색 텍스트 입력');
-    if (e.key === 'Enter') {
-      //입력한 검색어를 읽어와서 url 바꿔주기
-      e.preventDefault();
-      if (keyword.trim()) {
-        router.push(`/search/result?keyword=${encodeURIComponent(keyword)}`);
-      }
-    }
-  };
+//   // 전체 상품 목록 가져오기
+//   // useEffect(() => {
+//   //   const allProducts = async () => {
+//   //     try {
+//   //       const response = await fetch('api/products');
+//   //       const data = await response.json();
+//   //       setProducts(data);
+//   //     } catch (error) {
+//   //       console.log();
+//   //     }
+//   //   };
+//   // }, []);
 
-  const handleIconClick = (e: React.MouseEvent<HTMLButtonElement>) => {
-    e.preventDefault();
-    if (keyword.trim()) {
-      router.push(`/search/result?keyword=${encodeURIComponent(keyword)}`);
-    }
-  };
-  return (
-    <>
-      <form className=" border border-font-900 rounded-lg  flex items-center justify-between mt-2  tablet:mt-12 gap-2 text-font-400 mb-5">
-        {/* 검색 바 */}
-        <input
-          type="text"
-          onChange={e => setKeyword(e.target.value)}
-          onKeyDown={handleSearch}
-          placeholder="검색어를 입력해주세요."
-          className="w-full px-4 h-12 mobile:h-14 normal-14 tablet:text-[16px] outline-none"
-        />
-        <button type="submit" onClick={handleIconClick} className=" pr-[18px] cursor-pointer">
-          <Search className=" hover:text-font-900 w-4 h-4 tablet:w-5 tablet:h-5" />
-        </button>
-      </form>
-    </>
-  );
-}
+//   const handleSearch = (e: React.KeyboardEvent<HTMLInputElement>) => {
+//     // console.log('검색 텍스트 입력');
+//     if (e.key === 'Enter') {
+//       //입력한 검색어를 읽어와서 url 바꿔주기
+//       e.preventDefault();
+//       if (keyword.trim()) {
+//         router.push(`/search/result?keyword=${encodeURIComponent(keyword)}`);
+//       }
+//     }
+//   };
+
+//   const handleIconClick = (e: React.MouseEvent<HTMLButtonElement>) => {
+//     e.preventDefault();
+//     if (keyword.trim()) {
+//       router.push(`/search/result?keyword=${encodeURIComponent(keyword)}`);
+//     }
+//   };
+//   return (
+//     <>
+//       <form className=" border border-font-900 rounded-lg  flex items-center justify-between mt-2  tablet:mt-12 gap-2 text-font-400 mb-5">
+//         {/* 검색 바 */}
+//         <input
+//           type="text"
+//           onChange={e => setKeyword(e.target.value)}
+//           onKeyDown={handleSearch}
+//           placeholder="검색어를 입력해주세요."
+//           className="w-full px-4 h-12 mobile:h-14 normal-14 tablet:text-[16px] outline-none"
+//         />
+//         <button type="submit" onClick={handleIconClick} className=" pr-[18px] cursor-pointer">
+//           <Search className=" hover:text-font-900 w-4 h-4 tablet:w-5 tablet:h-5" />
+//         </button>
+//       </form>
+//     </>
+//   );
+// }
--- src/app/search/page.tsx
+++ src/app/search/page.tsx
@@ -1,37 +1,37 @@
-import { Footer } from '@components/common/Footer';
-import { ArrowLeft } from 'lucide-react';
+// import { Footer } from '@components/common/Footer';
+// import { ArrowLeft } from 'lucide-react';
 
-import { SearchFunction } from './SearchFunction';
+// import { SearchFunction } from '.SearchFunction';
 
-export default function SearchPage() {
-  return (
-    <>
-      {/* 모바일 버전: 뒤로가기 */}
-      <ArrowLeft className="m-3 tablet:hidden" size={20} />
+// export default function SearchPage() {
+//   return (
+//     <>
+//       {/* 모바일 버전: 뒤로가기 */}
+//       <ArrowLeft className="m-3 tablet:hidden" size={20} />
 
-      <div className=" mx-auto px-10 max-w-[1280px] mb-12 ">
-        <div className="flex flex-col w-full">
-          <SearchFunction />
-          <section className="flex justify-between px-3 medium-14 tablet:text-[16px] text-font-900 mb-[30px]">
-            <span>인기 검색어</span>
-            <span className="text-font-400">2025.08.08</span>
-          </section>
-          {/* 인기 목록 */}
-          <ol className="flex flex-col normal-14 tablet:text-[16px] gap-3 mobile:gap-5 list-decimal px-8">
-            <li>박선영은 최고야</li>
-            <li>나눈 코딩이 시러..</li>
-            <li>하지만</li>
-            <li>잘하고 싶어</li>
-            <li>개구리 중사 캐로캐로캐로캐로 티셔츠</li>
-            <li>타로카드</li>
-            <li>재밌는 보드게임</li>
-            <li>한복</li>
-            <li>생일선물</li>
-            <li>의미있는 선물</li>
-          </ol>
-        </div>
-      </div>
-      <Footer className="fixed bottom-0" />
-    </>
-  );
-}
+//       <div className=" mx-auto px-10 max-w-[1280px] mb-12 ">
+//         <div className="flex flex-col w-full">
+//           <SearchFunction />
+//           <section className="flex justify-between px-3 medium-14 tablet:text-[16px] text-font-900 mb-[30px]">
+//             <span>인기 검색어</span>
+//             <span className="text-font-400">2025.08.08</span>
+//           </section>
+//           {/* 인기 목록 */}
+//           <ol className="flex flex-col normal-14 tablet:text-[16px] gap-3 mobile:gap-5 list-decimal px-8">
+//             <li>박선영은 최고야</li>
+//             <li>나눈 코딩이 시러..</li>
+//             <li>하지만</li>
+//             <li>잘하고 싶어</li>
+//             <li>개구리 중사 캐로캐로캐로캐로 티셔츠</li>
+//             <li>타로카드</li>
+//             <li>재밌는 보드게임</li>
+//             <li>한복</li>
+//             <li>생일선물</li>
+//             <li>의미있는 선물</li>
+//           </ol>
+//         </div>
+//       </div>
+//       <Footer className="fixed bottom-0" />
+//     </>
+//   );
+// }

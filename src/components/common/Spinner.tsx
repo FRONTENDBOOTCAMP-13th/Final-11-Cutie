867759fda8e564368de246d88b9a1227b39c2fb4
Saetbyeol Bae <stella97415@gmail.com>
2025-07-31 09:08:18 +0900

    🔥 delete Spinner component

--- src/components/common/Spinner.tsx
+++
@@ -1,9 +0,0 @@
-import { SyncLoader } from "react-spinners";
-
-export default function Spinner(){
-  return(
-    <div className="flex justify-center items-center">
-      <SyncLoader color="#091fb0" />
-    </div>
-  )
-}

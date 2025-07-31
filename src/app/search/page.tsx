1f331548f54fa3300f79142b729410a7759fe197
오서현 <bbibbi1118@naver.com>
2025-07-31 13:03:58 +0900

    ✨ Component separation

--- src/app/search/page.tsx
+++ src/app/search/page.tsx
@@ -1,7 +1,7 @@
 import { Footer } from '@components/common/Footer';
 import { ArrowLeft } from 'lucide-react';
-
 import { SearchFunction } from './SearchFunction';
+import { PopularKeywords } from './PopularKeywords';
 
 export default function SearchPage() {
   return (
@@ -12,23 +12,7 @@ export default function SearchPage() {
       <div className=" mx-auto px-10 max-w-[1280px] mb-12 ">
         <div className="flex flex-col w-full">
           <SearchFunction />
-          <section className="flex justify-between px-3 medium-14 tablet:text-[16px] text-font-900 mb-[30px]">
-            <span>인기 검색어</span>
-            <span className="text-font-400">2025.08.08</span>
-          </section>
-          {/* 인기 목록 */}
-          <ol className="flex flex-col normal-14 tablet:text-[16px] gap-3 mobile:gap-5 list-decimal px-8">
-            <li>박선영은 최고야</li>
-            <li>나눈 코딩이 시러..</li>
-            <li>하지만</li>
-            <li>잘하고 싶어</li>
-            <li>개구리 중사 캐로캐로캐로캐로 티셔츠</li>
-            <li>타로카드</li>
-            <li>재밌는 보드게임</li>
-            <li>한복</li>
-            <li>생일선물</li>
-            <li>의미있는 선물</li>
-          </ol>
+          <PopularKeywords />
         </div>
       </div>
       <Footer className="fixed bottom-0" />

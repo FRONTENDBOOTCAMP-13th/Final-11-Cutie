488a086c4b83f122b9bd555a120fe3577d124180
kimlog126 <jisu11260@gmail.com>
2025-08-03 01:27:47 +0900

    ✨ input box add value props

--- src/components/form/form.tsx
+++ src/components/form/form.tsx
@@ -7,10 +7,12 @@ interface InputBoxProps {
   subtitle?: string;
   className?: string;
   setData?: (data: string) => void; // 데이터 저장 함수
+  value?: string; // input에 표시할 값 (수정 페이지 때문에 추가)
+  
 }
 
 /* 검색 태그 */
-export function InputBox({ placeholder, title, subtitle, className, setData }: InputBoxProps) {
+export function InputBox({ placeholder, title, subtitle, className, setData, value }: InputBoxProps) {
   return (
     <div className={`flex flex-col gap-[15px] text-[11px] ${className || ''} `}>
       <StarTitle
@@ -19,7 +21,7 @@ export function InputBox({ placeholder, title, subtitle, className, setData }: I
         className="max-[768px]:flex-col max-[768px]:items-start max-[480px]:gap-[4px] laptop:flex-col laptop:items-start"
       />
 
-      <InputIdResponsive placeholder={placeholder} setData={setData} />
+      <InputIdResponsive placeholder={placeholder} setData={setData} value={value} />
     </div>
   );
 }

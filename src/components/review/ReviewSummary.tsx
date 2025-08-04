487ef6a39998473f405d18c302afe331e9173de8
Saetbyeol Bae <stella97415@gmail.com>
2025-08-04 14:42:44 +0900

    💡 Remove unnecessary comments

--- src/components/review/ReviewSummary.tsx
+++ src/components/review/ReviewSummary.tsx
@@ -16,35 +16,6 @@ interface ReviewListProps {
   review: Review;
 }
 
-// 후기 테이블
-// export function ReviewListItem() {
-//   return (
-//     <div
-//       className="
-//       flex flex-col 
-//       w-[440px] 
-//       mobile:w-[568px]
-//       tablet:w-[880px] 
-//       laptop:w-[984px] 
-//       [@media(min-width:480px)_and_(max-width:768px)]:w-[470px]
-//       [@media(min-width:768px)_and_(max-width:900px)]:w-[680px]
-//       px-[16px]
-//     "
-//     >
-//       <div className="flex items-center gap-[10px]">
-//         <ProfileIcon className="w-[35px] h-[35px]" />
-//         <div className="flex flex-col">
-//           <span className="bold-14 laptop:text-[16px] text-font-900">닉네임</span>
-//           <div className="flex items-center ml-[-3px]">
-//             <span className="text-yellow-400 normal-14 laptop:text-[16px]">⭐</span>
-//             <span className="normal-16 text-font-900">5.0</span>
-//           </div>
-//         </div>
-//       </div>
-//       <p className="normal-14 laptop:text-[16px] text-font-900 mt-[10px]">대만족 합니다.</p>
-//     </div>
-//   );
-// }
 
 // 리뷰 후기
 
@@ -72,7 +43,7 @@ export default function ReviewImageList({ review }:ReviewListProps) {
       </div>
 
       {/* 이미지 목록 */}
-       {Array.isArray(review.extra.images) && review.extra.images.length > 0 && (
+       {Array.isArray(review.extra?.images) && review.extra?.images.length > 0 && (
         <div className="flex flex-nowrap gap-[4px] mt-[40px] mb-[1.5px] mobile:mb-[40px] overflow-hidden w-full">
           {review.extra.images.map((img, index) => (
             <Image

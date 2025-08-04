4c6b977b505d2090f726175cff18ea1380c3544e
오서현 <bbibbi1118@naver.com>
2025-08-04 10:28:30 +0900

    ✨ Check if logged in (accessToken present)

--- src/components/button/LikeBtn.tsx
+++ src/components/button/LikeBtn.tsx
@@ -19,22 +19,33 @@ export function ProductLikeBtn({
   const [isLiked, setIsLiked] = useState(initialIsLiked); // 현재 좋아요 상태
   const [bookmarkId, setBookmarkId] = useState<number | null>(initialBookmarkId || null); // 현재 북마크 ID(좋아요 추가 후 서버에서 받은 ID)
   const [isLoading, setIsLoading] = useState(false);
+  const accessToken = useUserStore(state => state.user?.token?.accessToken);
 
   // zutand에서 로그인 유저 정보 가져오기
   const { user } = useUserStore();
   const router = useRouter();
 
+  // 초기 상태 동기화
   useEffect(() => {
     setIsLiked(initialIsLiked);
     setBookmarkId(initialBookmarkId || null);
   }, [initialIsLiked, initialBookmarkId]);
 
+  /**
+   * 좋아요 버튼 클릭
+   */
   const handleToggle = (e: React.MouseEvent<HTMLButtonElement>) => {
-    e.stopPropagation();
-    e.preventDefault();
+    e.stopPropagation(); // 이벤트 전파 방지
+    e.preventDefault(); // 이벤트 전파 방지
     setIsLiked(!isLiked);
   };
 
+  // 로그인 여부 체크 (accessToken 유무)
+  if (!accessToken) {
+    const currentPath = window.location.pathname + window.location.search;
+    router.push(`/login?redirect=${encodeURIComponent(currentPath)}`);
+    return;
+  }
   return (
     <>
       <div className="absolute group right-4 bottom-4">

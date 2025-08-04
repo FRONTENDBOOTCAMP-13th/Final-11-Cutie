2dbf9c2df7d12e0e9cc5802f101d0fd6885ca90f
Saetbyeol Bae <stella97415@gmail.com>
2025-08-04 14:41:06 +0900

    ♻️ Refactor WriteReview page to support async searchParams

--- src/app/(main)/accounts/myReview/writeReview/page.tsx
+++ src/app/(main)/accounts/myReview/writeReview/page.tsx
@@ -1,16 +1,18 @@
 import WriteReviewForm from "@app/(main)/accounts/myReview/writeReview/WriteReviewForm";
 
 interface WriteReviewPageProps {
-  searchParams: { 
+  searchParams: Promise<{ 
     productId: string; 
     orderId: string;
-  };
+  }>;
 }
 
-export default function WriteReview({ searchParams }: WriteReviewPageProps) {
+export default async function WriteReview({ searchParams }: WriteReviewPageProps) {
   // URL 파라미터에서 값 추출 및 기본값 설정
-  const productId = parseInt(searchParams.productId);
-  const orderId = parseInt(searchParams.orderId);
+  const params = await searchParams;
+
+  const productId = parseInt(params.productId);
+  const orderId = parseInt(params.orderId);
 
   return (
     <WriteReviewForm

efc1f26dee8ebf53464e22e8c8a4c920522a65cd
kimlog126 <jisu11260@gmail.com>
2025-08-05 09:03:35 +0900

    🔧 fix goal percent util

--- src/utils/goalPercent.ts
+++ src/utils/goalPercent.ts
@@ -1,10 +1,5 @@
 import { Iproduct } from '@models/product';
 
-/**
- * 현재 달성률을 계산하는 함수
- * @param product - Iproduct 타입의 상품 객체
- * @returns goalPercent (0 ~ 100, 소수점 반올림)
- */
 export function calculateGoalPercent(product: Iproduct): number {
   const currentAmount = product.price * product.buyQuantity; // 현재 모금액
   const goalAmount = product.extra.goalPrice; // 목표 금액

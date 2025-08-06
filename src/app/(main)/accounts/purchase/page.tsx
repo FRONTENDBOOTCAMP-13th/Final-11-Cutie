8f56b2f4b5415f3b51e4dc4a077e2f400c931936
kimlog126 <jisu11260@gmail.com>
2025-08-07 02:44:01 +0900

    ♻️ account page refactoring

--- src/app/(main)/accounts/purchase/page.tsx
+++
@@ -1,9 +0,0 @@
-import PurchaseHistoryItemWrap from "@app/(main)/accounts/purchase/OrderHistory";
-
-export default function OrderHistoryPage(){
-  return(
-    <>
-       <PurchaseHistoryItemWrap /> 
-    </>
-  );
-}
